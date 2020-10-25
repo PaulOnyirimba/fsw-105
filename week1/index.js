c914310222ad8cbed42e4baa7ab90b8e57df0aad
PaulOnyirimba <Paul.Onyirimba@bryanuniversity.edu>
2020-10-25 16:09:14 -0400

    test

---
+++ week1/index.js
@@ -0,0 +1,17 @@
+var name = "Jason"
+var age = 24
+var isblack = true
+var isdead = false
+var person = {
+    name: "Peter",
+    age: 21,
+    eyecolor: "brown",
+    isalive: true
+}
+var numbers = [1, 2, 3, 4, 5]
+numbers
+var shopper = {
+    meat: "roastbeef",
+    bread: "italian",
+    milk; "2%"
+}
