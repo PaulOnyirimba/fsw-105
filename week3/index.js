3adc321af41bb070e5e821aeac7d49e1db8e89b1
PaulOnyirimba <Paul.Onyirimba@bryanuniversity.edu>
2020-11-05 15:21:05 -0500

    test

---
+++ week3/index.js
@@ -0,0 +1,45 @@
+var peoplwWhoWantToSeemadMaxFuryRoad =[
+    {
+        name: "Mike",
+        age: 12,
+        gender: "male"
+    } , {
+        name: "Madeline",
+        age: 80,
+        gender: "female"
+    } , {
+        name: "Cheryl",
+        age: 22,
+        gender: "female"
+    } , {
+        name: "Sam",
+        age: "30",
+        gender: "male"
+     } , {
+         name: "Suzy",
+         age: 4,
+         gender: "female"
+          }
+]
+if(12 >= 18){
+    console.log("Mike is old enough. He's good to see Mad Max Fury Road")
+} else if(12 < 18){
+    console.log("Mike is not old enough to see Mad Max Fury Road, dont let him in")
+}
+if(80 >= 18){
+    console.log("Madeline is old enough. She's good to see Mad Max Fury Road")
+} else if(80 < 18){
+    console.log("Madeline is not old enough to see Mad Max Fury Road, dont let her in")
+}
+if(22 >= 18){
+    console.log("Cheryl is old enough. She's good to see Mad Max Fury Road")
+}
+if(30 >= 18){
+    console.log("Sam is old enough. He's good to see Mad Max Fury Road")
+}
+if(4 < 18){
+    console.log("Suzy is not old enough to see Mad Max Fury Road, dont let her in")
+}
+for(var i = 0; i < 100; i++){
+    console.log(i)
+}
